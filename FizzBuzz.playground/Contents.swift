14deeafa1d7655e60bcfb180283a5a85960457c9
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2021-11-17 18:22:56 -0800

    FizzBuzz

---
+++ FizzBuzz.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+for i in 1...100 {
+    // Check for 3 and 5 first since it's the most specific
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
